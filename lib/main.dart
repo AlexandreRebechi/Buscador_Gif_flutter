635d22f23992d8c745d99a6ea3542a2473d42595
AlexandreRebechi <88150414+AlexandreRebechi@users.noreply.github.com>
2025-07-21 11:08:46 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: true,
+    debugShowCheckedModeBanner: false,
     home: HomePage(),
     theme: ThemeData(
       hintColor: Colors.white,
